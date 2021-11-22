4fb0172a0cd59e670264c35849e8c9ffd7ba3601
Andrew Hall <andewch0818@gmail.com>
2021-11-22 01:36:56 -0500

    added setters for possible implementation

--- record.cpp
+++ record.cpp
@@ -7,13 +7,13 @@ Record::Record(QString wform, float freq,int dur, int power)
     powerLevel=power;
     waveform=wform;
 }
+
 Record::Record()
 {
     frequency=0.0;
     duration=0;
     powerLevel=0;
     waveform="";
-
 }
 
 Record::~Record(){
@@ -41,6 +41,22 @@ int Record::getPowerLevel(){
     return powerLevel;
 }
 
+void Record::setFrequency(float freq){
+    frequency = freq;
+}
+
+void Record::setDuration(int time){
+    duration = time;
+}
+
+void Record::setPowerLevel(int power){
+    powerLevel=power;
+}
+
+void Record::setWaveForm(QString wForm){
+    waveform = wForm;
+}
+
 /*/
  * Function: Print()
  * Tentatively planning on it emitting a signal will probably change when we actually go about implementing.
@@ -48,7 +64,5 @@ int Record::getPowerLevel(){
  /*/
 QString Record::print(){
     QString report=("WAVEFORM: %s \n FREQUENCY: %f \n DURATION: %d seconds \n POWER LEVEL: %d",waveform,frequency,duration,QString::number(powerLevel));
-
     return report;
-
 }
