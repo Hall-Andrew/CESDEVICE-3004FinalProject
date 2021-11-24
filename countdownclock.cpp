e1b1cbb98a558f84a5c899a5dd9153f84c23acc9
Andrew Hall <andewch0818@gmail.com>
2021-11-24 01:48:17 -0500

    countDownClock class

--- countdownclock.cpp
+++ countdownclock.cpp
@@ -1,6 +1,39 @@
 #include "countdownclock.h"
 
-CountDownClock::CountDownClock()
+CountDownClock::CountDownClock(int number)
 {
+    minutes = number;
+    sDigitOne=0;
+    sDigitTwo;
+    finished=false;
+}
 
+void CountDownClock::setTimerNumber(int newTimerLength){
+    minutes = newTimerLength-1;
+    finished=false;
+}
+
+void CountDownClock::countdown(){
+    if(sDigitTwo != 0 ){
+        sDigitTwo-=1;
+    }else{
+        if(sDigitOne!=0){
+            sDigitOne-=1;
+        }else{
+            minutes-=1;
+            sDigitOne=5;
+            sDigitTwo=9;
+        }
+    }
+}
+
+QString CountDownClock::getDisplayNumbers(){
+    return QString::number(minutes)+":"+QString::number(sDigitOne)+QString::number(sDigitTwo);
+}
+
+bool CountDownClock::isTimerFinished(){
+    if(minutes==0 && sDigitOne==0 && sDigitTwo ==0 ){
+        finished=true;
+    }
+    return finished;
 }
--- countdownclock.h
+++ countdownclock.h
@@ -1,11 +1,22 @@
 #ifndef COUNTDOWNCLOCK_H
 #define COUNTDOWNCLOCK_H
-
+#include <QString>
 
 class CountDownClock
 {
-public:
-    CountDownClock();
+    public:
+        CountDownClock(int number);
+        void countdown();
+        void setTimerNumber(int newTimerLength);
+        QString getDisplayNumbers();
+        bool isTimerFinished();
+
+    private:
+        int minutes;
+        int sDigitOne;
+        int sDigitTwo;
+        bool finished;
+
 };
 
 #endif // COUNTDOWNCLOCK_H
